740eed45ede216253d58ef0211750bf8e137dd4c
V4d1ty <vadima52@inbox.lv>
2024-12-17 16:44:39 +0300

    first commit

---
+++ Itogovaya/Class.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class IsItPrime {
+    public static bool IsPrime(int n) {
+        if (n <= 1) return false;
+        for (int i = 2; i <= Math.Sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int[] Check(int n) {
+        int[] check = new int[100];
+        int count = 0;
+        for (int i = 2; i <= n; i++) {
+            while (n % i == 0) {
+                check[count] = i;
+                count++;
+                n /= i;
+            }
+        }
+
+        int[] result = new int[count];
+        for (int i = 0; i < count; i++) {
+            result[i] = check[i];
+        }
+
+        return result;
+    }
+}
---
+++ Itogovaya/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Program {
+    public static void Main(string[] args) {
+        Console.Write("Введите число: ");
+        if (int.TryParse(Console.ReadLine(), out int number)) {
+            if (number == 0) {
+                Console.WriteLine("0 не является ни простым, ни составным числом.");
+            }
+            else if (number == 1) {
+                Console.WriteLine("1 не является ни простым, ни составным числом."); 
+            }
+            else if (number < 0) {
+                Console.WriteLine($"{number} является отрицательным числом.");
+            }
+            else {
+                int absNumber = Math.Abs(number);
+                if (IsItPrime.IsPrime(absNumber)) {
+                    Console.WriteLine($"{number} является простым числом.");
+                }
+                else {
+                    Console.Write($"{number} является составным числом. Множители: ");
+                    int[] factors = IsItPrime.Check(absNumber);
+                    for (int i = 0; i < factors.Length; i++) {
+                        Console.Write(factors[i] + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+        else {
+            Console.WriteLine("Некорректный ввод.");
+        }
+    }
+}
